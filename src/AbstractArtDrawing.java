4ed2decf7fd925ceea8fdb40bc6de61672d42011
SorerBOT <alonfiller@gmail.com>
2024-05-30 23:47:42 +0300

    (Feat) now able to display lines on screen

---
+++ src/AbstractArtDrawing.java
@@ -0,0 +1,59 @@
+import biuoop.GUI;
+import biuoop.DrawSurface;
+import java.util.Random;
+import java.awt.Color;
+/**
+ * Compelled to create this comments by checkstyles.
+ */
+public class AbstractArtDrawing {
+    /**
+     * Generated random Lines.
+     * @param width the width of the screen
+     * @param height the height of the screen
+     * @param amountOfLines the amount of Lines to generate
+     * @return the Lines generated
+     */
+    public Line[] generateRandomLines(int width, int height, int amountOfLines) {
+        Random random = new Random();
+        Line[] lines = new Line[amountOfLines];
+        for (int i = 0; i < lines.length; i++) {
+            lines[i] = new Line(
+                random.nextInt(height) + 1,
+                random.nextInt(height) + 1,
+                random.nextInt(width) + 1,
+                random.nextInt(height) + 1
+            );
+        }
+        return lines;
+    }
+    /**
+     * Generates and draws random Lines.
+     * @param width the width of the screen
+     * @param height the height of the screen
+     * @param amountOfLines the amount of Lines
+     */
+    public void drawRandomLines(int width, int height, int amountOfLines) {
+        GUI gui = new GUI("Eye lineR", width, height);
+        DrawSurface drawSurface = gui.getDrawSurface();
+        Line[] lines = generateRandomLines(width, height, amountOfLines);
+
+        for (Line line : lines) {
+            drawSurface.setColor(Color.RED);
+            drawSurface.drawLine(
+                (int) line.start().getX(),
+                (int) line.start().getY(),
+                (int) line.end().getX(),
+                (int) line.end().getY()
+            );
+        }
+        gui.show(drawSurface);
+    }
+    /**
+     * Runs drawRandomCircles.
+     * @param args
+     */
+    public static void main(String[] args) {
+        AbstractArtDrawing runner = new AbstractArtDrawing();
+        runner.drawRandomLines(1920, 1080, 10);
+    }
+}
