dac3e9979217f0281af57491e6f48132ba8cda3a
SorerBOT <alonfiller@gmail.com>
2024-06-01 15:41:07 +0300

    (Patch) added length check to args

--- src/BouncingBallAnimation.java
+++ src/BouncingBallAnimation.java
@@ -7,6 +7,9 @@ public class BouncingBallAnimation {
      * @param args
      */
     public static void main(String[] args) {
+        if (args.length < 4) {
+            return;
+        }
         Point center = new Point(Double.parseDouble(args[0]), Double.parseDouble(args[1]));
         double dx = Double.parseDouble(args[2]);
         double dy = Double.parseDouble(args[3]);
---
+++ src/MultipleBouncingBallsAnimation.java
@@ -0,0 +1,9 @@
+public class MultipleBouncingBallsAnimation {
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
